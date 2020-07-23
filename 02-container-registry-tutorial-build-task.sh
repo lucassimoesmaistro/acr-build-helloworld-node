f3e19b59e4c82292a94dacba037554099678f66b
Lucas Simões Maistro <38821562+lucassimoesmaistro@users.noreply.github.com>
2020-07-23 17:16:09 -0300

    Update 02-container-registry-tutorial-build-task.sh

--- 02-container-registry-tutorial-build-task.sh
+++ 02-container-registry-tutorial-build-task.sh
@@ -2,7 +2,7 @@ ACR_NAME=acrlsmdev001
 RES_GROUP=rsg-$ACR_NAME
 AKV_NAME=$ACR_NAME-vault
 GIT_USER=lucassimoesmaistro
-GIT_PAT=92c1240656e783b87dc65818e8b7da46ddd0a1c6
+GIT_PAT=
 
 az acr task create \
     --registry $ACR_NAME \
