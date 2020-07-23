42716bb8d91440cdca565800c3a359759f4b5cd6
Lucas Simões <lucassimoes_@hotmail.com>
2020-07-23 17:41:02 -0300

    teste

--- 02-container-registry-tutorial-build-task.sh
+++ 02-container-registry-tutorial-build-task.sh
@@ -3,12 +3,17 @@ RES_GROUP=rsg-$ACR_NAME
 AKV_NAME=$ACR_NAME-vault
 GIT_USER=lucassimoesmaistro
 GIT_PAT=
-
+#88888_9_2_c_1240656e783b87dc65818e8b7da46ddd0a1c6
 az acr task create \
     --registry $ACR_NAME \
     --name taskhelloworld \
     --image helloworld:{{.Run.ID}} \
     --context https://github.com/$GIT_USER/acr-build-helloworld-node.git \
     --file Dockerfile \
+    --commit-trigger-enabled false \
+    --base-image-trigger-enabled true \
     --git-access-token $GIT_PAT
 
+#az acr task run --registry $ACR_NAME --name taskhelloworld
+#az acr task logs --registry $ACR_NAME
+#az acr task list-runs --registry $ACR_NAME --output table
