574a86a35cd2756d2512f19d24053a66f492b3da
Lucas Simões <lucassimoes_@hotmail.com>
2020-07-24 06:43:54 -0300

    Testing ACR Tasks

--- 02-container-registry-tutorial-build-task.sh
+++ 02-container-registry-tutorial-build-task.sh
@@ -2,8 +2,7 @@ ACR_NAME=acrlsmdev001
 RES_GROUP=rsg-$ACR_NAME
 AKV_NAME=$ACR_NAME-vault
 GIT_USER=lucassimoesmaistro
-GIT_PAT=
-#88888_9_2_c_1240656e783b87dc65818e8b7da46ddd0a1c6
+
 az acr task create \
     --registry $ACR_NAME \
     --name taskhelloworld \
